33f0ec6b30a003eff5cbf80ece01ab1f62c08e4e
jes57 <jasonstephens1084@gmail.com>
2014-05-17 16:06:14 -0400

    GUI Buttons using a class

--- src/bucky.py
+++ src/bucky.py
@@ -1,30 +1,36 @@
-# Simple GUI
+# Using a class to access tkinter
 
 from tkinter import *
 
-# Create the window
-main_window = Tk()
+class Application(Frame):
+    """ A GUI application with three buttons. """
 
-# Modify main_window window
-main_window.title("Labeler")
+    def __init__(self, master):
+        """ Initializes the Frame"""
+        Frame.__init__(self, master)
+        self.grid()
+        self.create_widgets()
+
+    def create_widgets(self):
+        """ Create 3 buttons that do nothing"""
+        # Create the first button
+        self.button1 = Button(self, text = "Button 1")
+        self.button1.grid()
+
+        # Second button
+        self.button2 = Button(self)
+        self.button2.grid()
+        self.button2.configure(text = "Button 2")
+
+        # Third button
+        self.button3 = Button(self)
+        self.button3.grid()
+        self.button3["text"] = "Button 3"
+main_window = Tk()
+main_window.title("Lazy buttons")
 main_window.geometry("400x200")
 
-# Creates the frame
-frame = Frame(main_window)
-frame.grid()      # Add the frame to the window
+# Use Application class in place of Frame class because we overloaded it
+app = Application(main_window)
 
-button1 = Button(frame, text = "Button 1")
-button1.grid()
-
-# Add text through the configure option
-button2 = Button(frame)
-button2.grid()
-button2.configure(text = "Button 2")
-
-# Add text in dictionary like fashion
-button3 = Button(frame)
-button3.grid()
-button3["text"] = "Button 3"
-
-# Kick off the event loop
 main_window.mainloop()
