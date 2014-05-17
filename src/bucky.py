dd8effa13ef94aedd3c8356791b54f9cba5dc4f3
jes57 <jasonstephens1084@gmail.com>
2014-05-17 16:11:38 -0400

    Binding and EventHandler

--- src/bucky.py
+++ src/bucky.py
@@ -9,23 +9,23 @@ class Application(Frame):
         """ Initializes the Frame"""
         Frame.__init__(self, master)
         self.grid()
+        self.button_clicks = 0  # count the number of button clicks
         self.create_widgets()
 
     def create_widgets(self):
-        """ Create 3 buttons that do nothing"""
+        """ Create 3 buttons that displays number of clicks"""
         # Create the first button
-        self.button1 = Button(self, text = "Button 1")
+        self.button1 = Button(self)
+        self.button1["text"] = "Total Clicks: 0"
+        self.button1["command"] = self.update_count # Bind event handler
         self.button1.grid()
 
-        # Second button
-        self.button2 = Button(self)
-        self.button2.grid()
-        self.button2.configure(text = "Button 2")
+    def update_count(self):
+        """ Increase the click count and display the new total"""
+        self.button_clicks += 1
+        self.button1["text"] = "Total Clicks: " + str(self.button_clicks)
 
-        # Third button
-        self.button3 = Button(self)
-        self.button3.grid()
-        self.button3["text"] = "Button 3"
+# Main program functionality        
 main_window = Tk()
 main_window.title("Lazy buttons")
 main_window.geometry("400x200")
