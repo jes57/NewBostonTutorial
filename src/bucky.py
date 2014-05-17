2d7de5d74192cb1f5eeb3414f2c26647bf1a38b6
jes57 <jasonstephens1084@gmail.com>
2014-05-17 15:50:21 -0400

    Label

--- src/bucky.py
+++ src/bucky.py
@@ -6,8 +6,15 @@ from tkinter import *
 root = Tk()
 
 # Modify root window
-root.title("Simple GUI")
-root.geometry("200x100")
+root.title("Labeler")
+root.geometry("200x50")
+
+# Creates the frame
+app = Frame(root)
+app.grid()      # Add the app to the window
+label = Label(app, text = "This is a label")
+label.grid()    # Add the label to the window
+
 
 # Kick off the event loop
 root.mainloop()
