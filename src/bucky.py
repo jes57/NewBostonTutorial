739fbce01f702b2666b444c576802cbce666692d
jes57 <jasonstephens1084@gmail.com>
2014-05-17 15:58:02 -0400

    Buttons

--- src/bucky.py
+++ src/bucky.py
@@ -3,18 +3,28 @@
 from tkinter import *
 
 # Create the window
-root = Tk()
+main_window = Tk()
 
-# Modify root window
-root.title("Labeler")
-root.geometry("200x50")
+# Modify main_window window
+main_window.title("Labeler")
+main_window.geometry("400x200")
 
 # Creates the frame
-app = Frame(root)
-app.grid()      # Add the app to the window
-label = Label(app, text = "This is a label")
-label.grid()    # Add the label to the window
+frame = Frame(main_window)
+frame.grid()      # Add the frame to the window
 
+button1 = Button(frame, text = "Button 1")
+button1.grid()
+
+# Add text through the configure option
+button2 = Button(frame)
+button2.grid()
+button2.configure(text = "Button 2")
+
+# Add text in dictionary like fashion
+button3 = Button(frame)
+button3.grid()
+button3["text"] = "Button 3"
 
 # Kick off the event loop
-root.mainloop()
+main_window.mainloop()
