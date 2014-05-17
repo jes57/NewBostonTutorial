974fc415c311fcdb6ae63b7963331629d2fe8082
jes57 <jasonstephens1084@gmail.com>
2014-05-17 15:46:44 -0400

    First GUI window. blank window

--- src/bucky.py
+++ src/bucky.py
@@ -1,2 +1,13 @@
-x=input("Enter name: ")
-print("Hello " + x)
+# Simple GUI
+
+from tkinter import *
+
+# Create the window
+root = Tk()
+
+# Modify root window
+root.title("Simple GUI")
+root.geometry("200x100")
+
+# Kick off the event loop
+root.mainloop()
